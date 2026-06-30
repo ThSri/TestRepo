5b6a27637ac8ebb79dd7d56feeb4000cf53021eb
Tharunasri Eegalapati <tharunas.eegalapa@smail.astate.edu>
2026-06-30 13:34:39 -0500

    Upload python file

---
+++ Practice.py
@@ -0,0 +1,76 @@
+print("Hello")
+
+name = "Tharuna"
+age = 21
+print(name)
+print(age)
+
+"""name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print("Hello", name)
+print("You are", age, "years old")
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
+
+name = input("Enter your name: ")
+language = input("which language do you like?: ")
+print("My name is", name)
+print("I like", language)
+
+num = int(input("Enter a number: "))
+if num > 0: 
+    print("The number is positive.")
+elif num < 0:
+    print("The number is negative.")
+else:    
+    print("The number is zero.")
+
+pwd = input("Enter your password: ")
+if pwd == "python123":
+    print("Access granted.")
+else:    
+    print("Access denied.")
+
+print(10 == 10)
+
+marks = int(input("Enter your marks: "))
+if marks >= 90:
+    print("Grade: A")
+elif marks >= 80:
+    print("Grade: B")
+elif marks >= 70:
+    print("Grade: C")
+else:
+    print("Grade: D")
+
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
+
+user = input("Enter your username: ")
+pwd = input("Enter your password: ")
+if pwd == "python123" and user == "admin":
+    print("Login successful.")
+else:    
+    print("Login failed.")
+
+num = int(input("Enter a number: "))
+if num >= 1 and num <= 100:
+    print("The number is valid.")
+else:    
+    print("The number is invalid.")
+
+name = input("Enter your name: ")
+print(name.upper())
+
+language = input("which language do you like?: ")
+print("I like " + language)
+
+print(len(name))
+print(name[0])
+print(name[6])"""
